d9a922052985c83005be67f5e541f82533a0fe15
Alysson <antonioalvesdasilvaalysson0@Gmail.com>
2026-05-11 08:45:45 -0300

    added function to read version part 1

--- main.js
+++ main.js
@@ -4,7 +4,17 @@ const path=require('path')
 const app = express();
 const port = 3000;
 const morgan = require('morgan');
+const { cache } = require('react');
 // Middleware para entender JSON (importante para APIs)
+// funções:
+
+async function fecthprs(url) {
+    let dado_receba= await fetch(url, {
+        cache: "no-store"
+    });
+    let final=await dado_receba.json();
+    return final;
+}
 app.use(express.json());
 app.use(express.static('client'));
 app.use(morgan('dev'));
@@ -12,11 +22,11 @@ app.use(morgan('dev'));
 let vezes=Number(0);
 app.get('/', (req, res) => {
     res.sendStatus(200);
-    console.log("alguem entrou")
+    console.log("alguem entrou");
 });
 // Exemplo de uma rota de API que retorna um objeto
 app.get('/status', (req, res) => {
-    
+    fecthprs("")//stop
 });
 app.get('/data', (req, res) => {
     const prg=req.query.dado;
